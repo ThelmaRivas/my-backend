0b1b2b906c3a1f22ea7dcaf827d1337a8571590e
ThelmaRivas <thelma.rivas00@gmail.com>
2023-12-29 15:33:13 -0600

    Added models and model index.js
    Add columns with specifies variables to the ProductTag, Product, Tag and Category Models. Import all models on index.js and establish the relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags',
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products',
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
